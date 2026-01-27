962442f844e537fa4b2adc3bf6885adf438b4796
이은솔 <essol503@gmail.com>
2026-01-27 19:48:01 +0900

    Initial Commit

---
+++ danbiTests/danbiTests.swift
@@ -0,0 +1,17 @@
+//
+//  danbiTests.swift
+//  danbiTests
+//
+//  Created by 이은솔 on 1/27/26.
+//
+
+import Testing
+@testable import danbi
+
+struct danbiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
